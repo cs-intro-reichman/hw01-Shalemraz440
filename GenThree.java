736548c2a1431831de5acc464a9883e25d403d3e
Shalem <shalem.raz@post.runi.ac.il>
2023-12-24 16:50:16 +0200

    Add awesome code for Homework 1

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,21 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int x = (int)(Math.random() * (b-a) + a);
+		int y = (int)(Math.random() * (b-a) + a);
+		int z = (int)(Math.random() * (b-a) + a);
+		int min = x;
+		if (y < min) {
+			min = y;
+		}
+		if (z < min) {
+			min = z;
+			}
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println("The minimal generated number was " + min);
 	}
 }
